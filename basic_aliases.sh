67355eab44c8b1a3a43af981631098f966971ad8
Mani Gillier <manigillier@gmail.com>
2024-12-10 21:00:03 +0100

    First commit with basic aliases

---
+++ basic_aliases.sh
@@ -0,0 +1,9 @@
+
+# Back move cd aliases
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Simple aliases
+alias c="clear"
---
+++ emacs_aliases.sh
@@ -0,0 +1,6 @@
+
+# Emacs no windows mode
+alias emacs="emacs -nw"
+
+# Magit
+alias magit="emacs -nw --eval 'magit'"
