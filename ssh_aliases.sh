cc3545f6fb316e8411ddf7b779093bb5203ad2cc
Mani Gillier <mani.gillier@epitech.eu>
2024-12-11 22:16:49 +0100

    [feat] Added agents

---
+++ ssh_aliases.sh
@@ -0,0 +1,8 @@
+
+# Use tty for pinentry in order to enter passphrases via my terminal instead of graphical interface
+export PINENTRY_BINARY="/usr/bin/pinentry-tty"
+export GPG_TTY='/dev/tty'
+
+# Launch the agents
+eval $(ssh-agent) > /dev/null
+gpg-agent --daemon --quiet 2> /dev/null
