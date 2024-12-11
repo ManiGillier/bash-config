b728c4f6f9b923127c92b68924ca7657748a306e
Mani Gillier <mani.gillier@epitech.eu>
2024-12-11 22:17:31 +0100

    [fix] gnupg tty is $(tty) not /dev/tty

--- ssh_aliases.sh
+++ ssh_aliases.sh
@@ -1,7 +1,7 @@
 
 # Use tty for pinentry in order to enter passphrases via my terminal instead of graphical interface
 export PINENTRY_BINARY="/usr/bin/pinentry-tty"
-export GPG_TTY='/dev/tty'
+export GPG_TTY=$(tty)
 
 # Launch the agents
 eval $(ssh-agent) > /dev/null
